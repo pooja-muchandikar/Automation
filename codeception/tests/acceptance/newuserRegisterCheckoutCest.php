9f8d8aa03b47a65d15f0e8b3b39b3b457792ffcb
pooja-muchandikar <pooja.muchandikar@rtcamp.com>
2020-01-28 18:31:16 +0530

    Update newuserRegisterCheckoutCest.php file

--- codeception/tests/acceptance/newuserRegisterCheckoutCest.php
+++ codeception/tests/acceptance/newuserRegisterCheckoutCest.php
@@ -13,19 +13,59 @@ class newuserRegisterCheckoutCest
         $I->seeElement('#menu-item-149021');
         $I->click('#menu-item-149021');
 
-        $I->scrollTo('.edd_purchase_submit_wrapper');
 
-        $I->seeElement('#edd_purchase_85270');
-        $I->click('#edd_purchase_85270');
+        $I->seeElement('#edd_purchase_116837');
+        $I->click('#edd_purchase_116837');
+        $I->seeElement('.edd_purchase_submit_wrapper ');
+        $I->click('.edd_purchase_submit_wrapper ');
 
-        $I->seeElement('.edd_purchase_submit_wrapper');
-        $I->click('.edd_purchase_submit_wrapper');
+        // $I->scrollTo('.edd_purchase_submit_wrapper');
 
-        echo "..........CHECKOUT PAGE.........";
+        // $I->seeElement('#edd_purchase_85270');
+        // $I->click('#edd_purchase_85270');
+
+        // $I->seeElement('.edd_purchase_submit_wrapper');
+        // $I->click('.edd_purchase_submit_wrapper');
+
+        // echo "..........CHECKOUT PAGE.........";
+        $I->wait(2);
+
         $I->amOnPage('/checkout');
 
+        $I->seeElement('#edd-email');
+        $I->fillField('#edd-email', 'poojatest@rtcamp.com');
 
-    
+        $I->seeElement('#edd-first');
+        $I->fillField('#edd-first', 'pooja');
+
+        $I->seeElement('#edd-last');
+        $I->fillField('#edd-last', 'm');
+
+        $I->seeElement('#edd_user_login');
+        $I->fillField('#edd_user_login', 'poojatest');
+
+        $I->seeElement('#edd_user_pass');
+        $I->fillField('#edd_user_pass', 'password');
+
+        $I->seeElement('#edd_user_pass_confirm');
+        $I->fillField('#edd_user_pass_confirm', 'password');
+
+        $I->click('#edd_show_terms');
+
+        $I->wait(2);
+
+        $I->seeElement('#edd_agree_to_terms');
+        $I->click('#edd_agree_to_terms');
+
+        $I->seeElement('#edd-purchase-button');
+        $I->click('#edd-purchase-button');
+
+        echo "..........Purchase History Page..........";
+
+        $I->wait(2);
+
+        
+  
     }
 }
 
